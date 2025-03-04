18a111bc1b520fee8989bffd3a47f16173ffb8e4
adamp <a.pastuszka@codeandpepper.com>
2025-03-04 19:58:18 +0100

    proof of concept for dashboard canvas graph

--- src/widget/dashboard.rs
+++ src/widget/dashboard.rs
@@ -1,6 +1,13 @@
 use iced::{
-    widget::{column, row, Text},
-    Element, Length,
+    event::Status,
+    mouse::{Cursor, Interaction},
+    widget::{
+        canvas::{self, Cache, Event, Geometry},
+        column, row, Canvas, Text,
+    },
+    Element,
+    Length::{self, Fill},
+    Rectangle, Renderer, Size, Theme,
 };
 
 use crate::app::{Context, Message};
@@ -17,17 +24,22 @@ impl From<DashboardMessage> for Message {
 }
 
 #[derive(Debug)]
-pub struct Dashboard {}
+pub struct Dashboard {
+    cache: Cache,
+}
 
 impl Dashboard {
     pub fn new() -> Self {
-        Dashboard {}
+        Dashboard {
+            cache: Cache::new(),
+        }
     }
 }
 
 impl Widget for Dashboard {
     fn view(&self) -> Element<Message> {
-        let content = column![Text::new("Dashboard").size(40)].spacing(10);
+        let canvas = Canvas::new(self).width(Fill).height(Fill);
+        let content = column![Text::new("Dashboard").size(40), canvas].spacing(10);
 
         row![sidebar(), content]
             .height(Length::Fill)
@@ -37,4 +49,47 @@ impl Widget for Dashboard {
     }
 
     fn update(&mut self, _ctx: &mut Context, _msg: Message) {}
+}
+
+impl canvas::Program<Message> for Dashboard {
+    type State = ();
+
+    fn update(
+        &self,
+        _state: &mut Self::State,
+        _event: Event,
+        _bounds: Rectangle,
+        _cursor: Cursor,
+    ) -> (Status, Option<Message>) {
+        (Status::Captured, None)
+    }
+
+    fn draw(
+        &self,
+        _state: &Self::State,
+        renderer: &Renderer,
+        theme: &Theme,
+        bounds: Rectangle,
+        _cursor: Cursor,
+    ) -> Vec<Geometry<Renderer>> {
+        let text_color = theme.extended_palette().primary.base.color;
+        let graph_fill = canvas::Fill {
+            style: canvas::Style::Solid(text_color),
+            ..Default::default()
+        };
+
+        let graph = self.cache.draw(renderer, bounds.size(), |frame| {
+            frame.fill_rectangle(frame.center(), Size::new(10.0, 10.0), graph_fill);
+        });
+        vec![graph]
+    }
+
+    fn mouse_interaction(
+        &self,
+        _state: &Self::State,
+        _bounds: Rectangle,
+        _cursor: Cursor,
+    ) -> Interaction {
+        Interaction::None
+    }
 }
