1a5267d60bfaf14aeb1af32f13f68916a141a546
Neidz <adamo7255@gmail.com>
2025-07-11 20:08:58 +0200

    fix install script

--- script/install-linux.sh
+++ script/install-linux.sh
@@ -3,7 +3,7 @@
 DESKTOP_FILE="Chomp.desktop"
 DEST_DIR="$HOME/.local/share/applications"
 
-cargo install --path .
+cargo install --path ./chomp-app
 mkdir -p "$DEST_DIR"
 cp "$DESKTOP_FILE" "$DEST_DIR/"
 chmod +x "$DEST_DIR/$DESKTOP_FILE"
