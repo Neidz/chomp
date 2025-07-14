3f4ffda3328235bb0fba798b9df4a1ad22ba1cae
Neidz <adamo7255@gmail.com>
2025-07-14 21:16:35 +0200

    add sidebar icons

--- chomp-app/src/widget/sidebar.rs
+++ chomp-app/src/widget/sidebar.rs
@@ -1,27 +1,71 @@
 use iced::{
-    widget::{column, Button, Column},
+    widget::{column, row, svg, Button, Column},
     Element, Length,
 };
 
 use crate::app::{Message, NextWidget};
 
 pub fn sidebar() -> Element<'static, Message> {
+    let dashboard_icon = svg::Handle::from_path(format!(
+        "{}/resources/chart-pie.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+    let product_icon =
+        svg::Handle::from_path(format!("{}/resources/beef.svg", env!("CARGO_MANIFEST_DIR")));
+    let meal_icon = svg::Handle::from_path(format!(
+        "{}/resources/salad.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+    let weight_icon = svg::Handle::from_path(format!(
+        "{}/resources/weight.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+    let target_icon = svg::Handle::from_path(format!(
+        "{}/resources/target.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+    let tool_icon = svg::Handle::from_path(format!(
+        "{}/resources/wrench.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+
     let buttons = vec![
-        ("Dashboard", Message::ChangeWidget(NextWidget::Dashboard)),
-        ("Meals", Message::ChangeWidget(NextWidget::MealList)),
-        ("Products", Message::ChangeWidget(NextWidget::ProductList)),
-        ("Weights", Message::ChangeWidget(NextWidget::WeightList)),
         (
+            dashboard_icon.clone(),
+            "Dashboard",
+            Message::ChangeWidget(NextWidget::Dashboard),
+        ),
+        (
+            meal_icon.clone(),
+            "Meals",
+            Message::ChangeWidget(NextWidget::MealList),
+        ),
+        (
+            product_icon.clone(),
+            "Products",
+            Message::ChangeWidget(NextWidget::ProductList),
+        ),
+        (
+            weight_icon.clone(),
+            "Weights",
+            Message::ChangeWidget(NextWidget::WeightList),
+        ),
+        (
+            target_icon.clone(),
             "Nutrition Targets",
             Message::ChangeWidget(NextWidget::NutritionTargetList),
         ),
-        ("Tools", Message::ChangeWidget(NextWidget::Tools)),
+        (
+            tool_icon.clone(),
+            "Tools",
+            Message::ChangeWidget(NextWidget::Tools),
+        ),
     ];
 
     let navigation = buttons
         .into_iter()
-        .map(|(content, message)| {
-            Button::new(content)
+        .map(|(icon, content, message)| {
+            Button::new(row![svg(icon).width(20.0), content].spacing(10.0))
                 .on_press(message)
                 .width(Length::Fill)
                 .into()
