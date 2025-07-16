f7323208897f4af53b86e0b3544859cc182fd6e5
Neidz <adamo7255@gmail.com>
2025-07-16 18:34:27 +0200

    add product portion service

--- chomp-services/src/lib.rs
+++ chomp-services/src/lib.rs
@@ -10,16 +10,21 @@ mod error;
 mod meals;
 mod nutrition_target;
 mod product;
+mod product_portion;
 mod weight;
 
 pub use error::ServiceError;
 pub use meals::{AddMealProduct, Meal, MealDayStats, MealProduct, UpdateMealProductWeight};
 pub use nutrition_target::NutritionTarget;
 pub use product::{CreateUpdateProduct, Product};
+pub use product_portion::{CreateProductPortion, ProductPortion};
 pub use weight::Weight;
+
+use crate::product_portion::ProductPortionService;
 
 pub struct Services {
     pub product: ProductService,
+    pub product_portions: ProductPortionService,
     pub weight: WeightService,
     pub meal: MealService,
     pub nutrition_target: NutritionTargetService,
@@ -30,12 +35,14 @@ impl Services {
         let db_rc = Rc::new(RefCell::new(db));
 
         let product = ProductService::new(db_rc.clone());
+        let product_portions = ProductPortionService::new(db_rc.clone());
         let weight = WeightService::new(db_rc.clone());
         let meal = MealService::new(db_rc.clone());
         let nutrition_target = NutritionTargetService::new(db_rc.clone());
 
         Services {
             product,
+            product_portions,
             weight,
             meal,
             nutrition_target,
---
+++ chomp-services/src/product_portion.rs
@@ -0,0 +1,124 @@
+use std::{cell::RefCell, rc::Rc};
+
+use rusqlite::{params, Connection};
+use serde::Deserialize;
+
+use super::ServiceError;
+
+#[derive(Debug, Clone)]
+pub struct ProductPortion {
+    pub id: usize,
+    pub name: String,
+    pub product_id: usize,
+    pub weight: f32,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct CreateProductPortion {
+    pub name: String,
+    pub product_id: usize,
+    pub weight: f32,
+}
+
+pub struct ProductPortionService {
+    db: Rc<RefCell<Connection>>,
+}
+
+impl ProductPortionService {
+    pub fn new(db: Rc<RefCell<Connection>>) -> Self {
+        ProductPortionService { db }
+    }
+
+    pub fn create(&self, product_portion: CreateProductPortion) -> Result<(), ServiceError> {
+        let query = "
+            INSERT INTO product_portions (name, product_id, weight)
+    	    VALUES (?1, ?2, ?3)";
+        let args = params![
+            product_portion.name,
+            product_portion.product_id,
+            product_portion.weight
+        ];
+
+        let db = self.db.borrow();
+        let mut stmt = db.prepare(query)?;
+        stmt.execute(args).map_err(ServiceError::from)?;
+
+        Ok(())
+    }
+
+    pub fn update(&self, id: usize, product_portion: ProductPortion) -> Result<(), ServiceError> {
+        let query = "
+            UPDATE product_portions
+            SET name=?1, weight=?2
+            WHERE id = ?3";
+        let args = params![
+            product_portion.name,
+            product_portion.weight,
+            product_portion.id,
+        ];
+
+        let db = self.db.borrow();
+        let mut stmt = db.prepare(query)?;
+        stmt.execute(args).map_err(ServiceError::from)?;
+
+        Ok(())
+    }
+
+    pub fn read(&self, id: usize) -> Result<ProductPortion, ServiceError> {
+        let query = "
+            SELECT id, name, product_id, weight
+            FROM product_portions
+    		WHERE id = ?1";
+        let args = params![id];
+
+        let db = self.db.borrow();
+        let mut stmt = db.prepare(query)?;
+
+        stmt.query_row(args, |row| {
+            Ok(ProductPortion {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                product_id: row.get(2)?,
+                weight: row.get(3)?,
+            })
+        })
+        .map_err(ServiceError::from)
+    }
+
+    pub fn delete(&self, id: usize) -> Result<(), ServiceError> {
+        let query = "
+            DELETE FROM product_portions
+    	    WHERE id = ?1";
+        let args = params![id];
+
+        let db = self.db.borrow();
+        let mut stmt = db.prepare(query)?;
+        stmt.execute(args).map_err(ServiceError::from)?;
+
+        Ok(())
+    }
+
+    pub fn list(&self, product_id: usize) -> Result<Vec<ProductPortion>, ServiceError> {
+        let query = "
+            SELECT id, name, product_id, weight
+            FROM product_portions
+            ORDER BY id ASC";
+
+        let db = self.db.borrow();
+        let mut stmt = db.prepare(query)?;
+
+        let product_portions = stmt
+            .query_map([], |row| {
+                Ok(ProductPortion {
+                    id: row.get(0)?,
+                    name: row.get(1)?,
+                    product_id: row.get(2)?,
+                    weight: row.get(3)?,
+                })
+            })
+            .map_err(ServiceError::from)?
+            .collect::<Result<Vec<ProductPortion>, _>>()?;
+
+        Ok(product_portions)
+    }
+}
